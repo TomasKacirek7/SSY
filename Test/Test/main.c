53f69b615f1be7a7d9537495b02607b75a01bae8
TomasKacirek7 <241044@vutbr.cz>
2025-02-17 14:43:02 +0100

    Add files via upload

---
+++ Test/Test/main.c
@@ -0,0 +1,21 @@
+/*
+ * Test.c
+ *
+ * Created: 2/17/2025 12:23:11
+ * Author : Student
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+
+volatile int a=5;
+
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
