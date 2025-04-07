e8d77b5382b8d4e3122591f31344b75f2e992cff
TomasKacirek7 <241044@vutbr.cz>
2025-04-07 15:18:52 +0200

    Add files via upload

---
+++ Strana A/Com4-A/LWM_MSSY/main.c
@@ -0,0 +1,222 @@
+/*
+ * LWM_MSSY.c
+ *
+ * Created: 6.4.2017 15:42:46
+ * Author : Krajsa
+ */
+
+#include <avr/io.h>
+/*- Includes ---------------------------------------------------------------*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "config.h"
+#include "hal.h"
+#include "phy.h"
+#include "sys.h"
+#include "nwk.h"
+#include "sysTimer.h"
+#include "halBoard.h"
+#include "halUart.h"
+#include "main.h"
+
+/*- Definitions ------------------------------------------------------------*/
+#ifdef NWK_ENABLE_SECURITY
+#define APP_BUFFER_SIZE    (NWK_MAX_PAYLOAD_SIZE - NWK_SECURITY_MIC_SIZE)
+#else
+#define APP_BUFFER_SIZE    NWK_MAX_PAYLOAD_SIZE
+#endif
+/*- Types ------------------------------------------------------------------*/
+typedef enum AppState_t
+{
+	APP_STATE_INITIAL,
+	APP_STATE_IDLE,
+} AppState_t;
+
+/*- Prototypes -------------------------------------------------------------*/
+static void appSendData(void);
+
+void board_init();
+uint32_t mod_exp(uint32_t base, uint32_t exp, uint32_t mod);
+void print_result(const char* label, uint32_t value);
+
+/*- Variables --------------------------------------------------------------*/
+static AppState_t appState = APP_STATE_INITIAL;
+static SYS_Timer_t appTimer;
+static NWK_DataReq_t appDataReq;
+static bool appDataReqBusy = false;
+static uint8_t appDataReqBuffer[APP_BUFFER_SIZE];
+static uint8_t appUartBuffer[APP_BUFFER_SIZE];
+static uint8_t appUartBufferPtr = 0;
+
+/*- Implementations --------------------------------------------------------*/
+
+/*************************************************************************//**
+*****************************************************************************/
+static void appDataConf(NWK_DataReq_t *req)
+{
+appDataReqBusy = false;
+(void)req;
+}
+
+
+/*************************************************************************//**
+*****************************************************************************/
+static void appSendData(void)
+{
+if (appDataReqBusy || 0 == appUartBufferPtr)
+return;
+
+memcpy(appDataReqBuffer, appUartBuffer, appUartBufferPtr);
+
+appDataReq.dstAddr = 1-APP_ADDR;
+appDataReq.dstEndpoint = APP_ENDPOINT;
+appDataReq.srcEndpoint = APP_ENDPOINT;
+appDataReq.options = NWK_OPT_ENABLE_SECURITY;
+appDataReq.data = appDataReqBuffer;
+appDataReq.size = appUartBufferPtr;
+appDataReq.confirm = appDataConf;
+NWK_DataReq(&appDataReq);
+
+appUartBufferPtr = 0;
+appDataReqBusy = true;
+}
+
+/*************************************************************************//**
+*****************************************************************************/
+void HAL_UartBytesReceived(uint16_t bytes)
+{
+for (uint16_t i = 0; i < bytes; i++)
+{
+uint8_t byte = HAL_UartReadByte();
+
+if (appUartBufferPtr == sizeof(appUartBuffer))
+appSendData();
+
+if (appUartBufferPtr < sizeof(appUartBuffer))
+appUartBuffer[appUartBufferPtr++] = byte;
+}
+
+SYS_TimerStop(&appTimer);
+SYS_TimerStart(&appTimer);
+}
+
+/*************************************************************************//**
+*****************************************************************************/
+static void appTimerHandler(SYS_Timer_t *timer)
+{
+appSendData();
+(void)timer;
+}
+
+/*************************************************************************//**
+*****************************************************************************/
+static bool appDataInd(NWK_DataInd_t *ind)
+{
+for (uint8_t i = 0; i < ind->size; i++)
+HAL_UartWriteByte(ind->data[i]);
+return true;
+}
+
+/*************************************************************************//**
+*****************************************************************************/
+static void appInit(void)
+{
+NWK_SetAddr(APP_ADDR);
+NWK_SetPanId(APP_PANID);
+PHY_SetChannel(APP_CHANNEL);
+#ifdef PHY_AT86RF212
+PHY_SetBand(APP_BAND);
+PHY_SetModulation(APP_MODULATION);
+#endif
+PHY_SetRxState(true);
+
+NWK_OpenEndpoint(APP_ENDPOINT, appDataInd);
+
+HAL_BoardInit();
+
+appTimer.interval = APP_FLUSH_TIMER_INTERVAL;
+appTimer.mode = SYS_TIMER_INTERVAL_MODE;
+appTimer.handler = appTimerHandler;
+}
+
+/*************************************************************************//**
+*****************************************************************************/
+static void APP_TaskHandler(void)
+{
+switch (appState)
+{
+case APP_STATE_INITIAL:
+{
+appInit();
+appState = APP_STATE_IDLE;
+} break;
+
+case APP_STATE_IDLE:
+break;
+
+default:
+break;
+}
+}
+
+/*************************************************************************//**
+*****************************************************************************/
+int main(void)
+{
+
+	// Uvodni text pro Alici
+	printf("Diffie-Hellman Exchange - Alice\n\r");
+
+	// Parametry (stejne jako u Boba)
+	uint8_t P = 23; // P must be prime
+	uint8_t G = 5;  // G is a primitive root modulo P
+	
+
+	// Tajny klic pro Alici
+	uint8_t secret_a = 7;  // Tajny klic pro Alici
+	
+
+	// Vypocet verejneho klice Alice
+	uint8_t public_A = mod_exp(G, secret_a, P);
+	
+
+	SYS_Init();
+	HAL_UartInit(38400);
+
+	// Odeslani verejneho klice Alice pres UART jako ASCII znak
+	// Zajistime, ze hodnota je v rozsahu 97-122 ('a' - 'z')
+	uint8_t char_public_A = (public_A) + 'a';
+	
+	// Zavoláme funkci pro odeslání dat
+	appSendData();
+
+
+	while (1)
+	{
+		SYS_TaskHandler();
+		HAL_UartTaskHandler();
+		APP_TaskHandler();
+		
+	}
+}
+
+/*************************************************************************//**
+ *****************************************************************************/
+uint32_t mod_exp(uint32_t base, uint32_t exp, uint32_t mod) {
+	uint32_t res = 1;
+	base = base % mod;
+	while (exp > 0) {
+		if (exp % 2 == 1) res = (res * base) % mod;
+		base = (base * base) % mod;
+		exp /= 2;
+	}
+	return res;
+}
+
+/*************************************************************************//**
+ *****************************************************************************/
+void print_result(const char* label, uint32_t value) {
+	printf("%s %lu\n\r", label, value);
+}
